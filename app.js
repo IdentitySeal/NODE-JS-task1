a4a5be943c16d2996bfd8eebc23fed2ad7e35dea
IdentitySeal <teemealeheen@gmail.com>
2020-03-26 22:52:00 +0100

    A Simple Form and Submit to a File

--- app.js
+++ app.js
@@ -1,26 +1,31 @@
 const http = require('http');
+const fs = require('fs');
 const { parse } = require('querystring');
 
 const server = http.createServer((req, res) => {
     if (req.method === 'POST') {
         collectRequestData(req, result => {
-            console.log(result);
-            res.end(`
-            <!doctype html>
-            <html>
-            <body>
-            <h2>See the message you sent : </h2>
-            <pre>${result.message}</pre>
-           </body>
-            </html>
-             `);
+            fs.writeFile('message.txt', result.message, (err) => {
+                if (err) throw err;
+                console.log('It has been Saved');
+            })
+
+            res.end(
+                ` <!doctype html>
+                    <html>
+                    <body>
+                    <h2>See the message you sent : </h2>
+                    <pre>${result.message}</pre>
+                   </body>
+                    </html>`
+            );
         });
     } else {
         res.end(`
             <!doctype html>
             <html>
             <body>
-                <form action="/" method="post" id ="usrform" >
+                <form action="/message" method="post" id ="usrform" >
                     <textarea name="message" form="usrform">Enter message here...</textarea>
                     <button>Submit</button>
                 </form>
@@ -29,7 +34,7 @@ const server = http.createServer((req, res) => {
         `);
     }
 });
-server.listen(3000);
+server.listen(8080);
 
 function collectRequestData(request, callback) {
     const FORM_URLENCODED = 'application/x-www-form-urlencoded';
